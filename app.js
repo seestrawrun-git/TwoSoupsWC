207292fe41fcc348e27de815b81b5f30caf205e6
seestrawrun <css9729@nyu.edu>
2023-08-05 16:14:40 -0400

    Added images and links

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
