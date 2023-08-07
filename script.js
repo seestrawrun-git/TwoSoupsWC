77c0203a0e02a23bc2798f73151a5e328baac6e4
seestrawrun <css9729@nyu.edu>
2023-08-07 14:10:44 -0400

    added 3d model to about.html

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 //three js components
 let scene, camera, renderer, clock, mixer, clips
-let cameraPosition = new THREE.Vector3(5, 5, 30)
+let cameraPosition = new THREE.Vector3(0, 0, 20)
 let model
 
 let divCanvas = document.querySelector('#three');
 
-console.log(divCanvas.clientWidth, divCanvas.clientHeight);
 
+console.log(divCanvas.clientHeight + " " + divCanvas.clientWidth);
 function init() {
   //clock
   clock = new THREE.Clock()
 
   scene = new THREE.Scene()
-  camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera = new THREE.PerspectiveCamera(15, divCanvas.clientWidth / divCanvas.clientHeight, 0.1, 1000)
   camera.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z)
-  camera.lookAt(0, 0, 0)
+  camera.lookAt(0, 2.5, 0)
   //set up renderer
   renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#three'),
@@ -36,6 +36,7 @@ function init() {
   scene.add(directionalLight, ambientLight)
 
   //add some cubes
+  /*
   const floorGeometry = new THREE.BoxGeometry(5, 0.1, 5)
   const material = new THREE.MeshStandardMaterial({ color: 0x99bbee })
   const floor = new THREE.Mesh(floorGeometry, material)
@@ -45,7 +46,7 @@ function init() {
   const back = new THREE.Mesh(wallGeometry, material)
   back.position.set(0, 1.5, -2.5)
 
-  scene.add(floor, back)
+  scene.add(floor, back)*/
 
   //add your model
   // create a animation mixer
@@ -56,15 +57,17 @@ function init() {
   // Load a glTF resource
   loader.load(
     // resource URL
-    'faceless.glb',
+    'idle.glb',
     // called when the resource is loaded
     function(gltf) {
       model = gltf.scene
       scene.add(model);
+      model.scale.set(2.4, 2.4, 2.4);
 
       mixer = new THREE.AnimationMixer(model);
       clips = gltf.animations; // Array<THREE.AnimationClip>
-      mixer.clipAction(clips[2]).play()
+      mixer.clipAction(clips[0]).play()
+      
     },
     // called while loading is progressing
     function(xhr) {
@@ -77,10 +80,88 @@ function init() {
   );
 
   window.onresize = function() {
-    camera.aspect = window.innerWidth / window.innerHeight
+    console.log(divCanvas.clientHeight + " " + divCanvas.clientWidth);
+     
+    camera.aspect = (window.innerWidth/ window.innerHeight)
     camera.updateProjectionMatrix()
     renderer.setSize(divCanvas.clientWidth, divCanvas.clientHeight)
   }
+}
+
+let previousMousePosition = {
+x: 0,
+y: 0
+};
+
+let touchStartX = 0;
+let touchStartY = 0;
+let rotationSpeed = 0.01;
+
+let isDragging = false;
+
+document.addEventListener('mousedown', onMouseDown);
+document.addEventListener('mousemove', onMouseMove);
+document.addEventListener('mouseup', onMouseUp);
+document.addEventListener('touchstart', onTouchStart, false);
+document.addEventListener('touchmove', onTouchMove, false);
+
+function onMouseDown(event) {
+  isDragging = true;
+  previousMousePosition = {
+    x: event.clientX,
+    y: event.clientY
+  };
+}
+
+function onMouseMove(event) {
+  if (!isDragging) return;
+
+  const deltaX = event.clientX - previousMousePosition.x;
+  const deltaY = event.clientY - previousMousePosition.y;
+
+  rotateModel(deltaX, deltaY);
+
+  previousMousePosition = {
+    x: event.clientX,
+    y: event.clientY
+  };
+}
+
+function onMouseUp() {
+  isDragging = false;
+}
+
+function onTouchStart(event) {
+  touchStartX = event.touches[0].clientX;
+  touchStartY = event.touches[0].clientY;
+}
+
+function onTouchMove(event) {
+  if (event.touches.length === 1) {
+    const touchX = event.touches[0].clientX;
+    const touchY = event.touches[0].clientY;
+
+    const deltaX = touchX - touchStartX;
+    const deltaY = touchY - touchStartY;
+
+    // Rotate the model based on touch movement
+    model.rotation.y += deltaX * rotationSpeed;
+    model.rotation.x += 0 * rotationSpeed;
+
+    touchStartX = touchX;
+    touchStartY = touchY;
+  }
+}
+
+function rotateModel(deltaX, deltaY) {
+  // Adjust the rotation speed
+  const rotationSpeed = 0.005;
+
+  if (!model) return;
+
+  // Rotate the model
+  model.rotation.y += deltaX * rotationSpeed;
+  model.rotation.x += 0 * rotationSpeed;
 }
 
 function animate() {
@@ -91,6 +172,10 @@ function animate() {
   }
   renderer.render(scene, camera)
 }
+
+
+
+
 
 init()
 animate()
